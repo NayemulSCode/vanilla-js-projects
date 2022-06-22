205be68e93dbea4385324b16ccda9f654c3b5efd
NayemulSCode <nayemulhasan2016@gmail.com>
2022-06-22 23:39:25 +0600

    make money double with button click

--- array-dom/scripts.js
+++ array-dom/scripts.js
@@ -1,8 +1,9 @@
 const P = console.log.bind(console)
 const main = document.getElementById('main');
 const createUserButton = document.getElementById('create-user');
-
+const doubelMoneyBotton = document.getElementById('double-money');
 let data = [];
+// at a time 2 user show very first load;
 getRandomUser();
 
 async function getRandomUser(){
@@ -14,29 +15,44 @@ async function getRandomUser(){
         money: Math.floor(Math.random() * 1000000)
     };
     // P('user info', newUser)
-    addData(newUser)
-
+    addData(newUser);
 }
 //create / add object to data array 
-
 function addData(obj) {
     data.push(obj);
     updateDOM();
 }
+
+// double everyones money
+function doubelMoney(){
+    data = data.map(user =>{
+        return {
+            ...user,
+            money: user.money * 2
+        }
+    });
+    // P('double money', data)
+    updateDOM()
+}
+
 //update DOM
 function updateDOM(providedData = data){
     main.innerHTML = '<h2>Person Wealth</h2>';
     providedData.forEach(item =>{
-        P(item);
+        // P(item);
         const element = document.createElement('div');
         element.classList.add('person');
         element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`
         main.appendChild(element);
     });
 }
+
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
 function formatMoney(number){
     return '৳' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
 // event listeners
 createUserButton.addEventListener('click', getRandomUser)
+doubelMoneyBotton.addEventListener('click', doubelMoney)
